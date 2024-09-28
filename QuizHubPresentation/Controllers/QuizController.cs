41e7c032e8d0c78cb45df0f136346d3ae971286e
Seçil Seleci <143125124+secilseleci@users.noreply.github.com>
2024-09-28 15:53:00 +0300

    Pending Quizzes page is done

---
+++ QuizHubPresentation/Controllers/QuizController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Contracts;
+using Services.Contracts;
+
+namespace QuizHubPresentation.Controllers
+{
+    public class QuizController:Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IRepositoryManager _manager;
+        private readonly IMapper _mapper;
+        public QuizController(IRepositoryManager manager, IMapper mapper, UserManager<ApplicationUser> userManager)
+        {
+            _manager = manager;
+            _mapper = mapper;
+            _userManager = userManager;
+
+        }
+        public IActionResult PendingQuizzes()
+        {
+            return View(); // pendingQuizzes view'ına yönlendirilir
+        }
+
+        public IActionResult CompletedQuizzes()
+        {
+            return View(); // pendingQuizzes view'ına yönlendirilir
+        }
+
+        public IActionResult RetakeQuizzes()
+        {
+            return View(); // pendingQuizzes view'ına yönlendirilir
+        }
+    }
+}
